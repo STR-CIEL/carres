573b0f93d32c1b0c63d1aef5d9179011ac8d64e3
Besim Strazimiri <besimstrazimiri@outlook.fr>
2025-01-08 20:24:14 +0100

    test

---
+++ Doc.md
@@ -0,0 +1,24 @@
+\mainpage Page principale de la documentation
+[TOC]
+# TP SNIR Les Threads
+## Objectifs
+- Comprendre les threads en C++.
+## Modélisation UML
+- Utilisation de MagicDraw.
+## Exemples Markdown
+```cpp
+int main() {
+    /// @brief Exemple de variable
+    int var = 10;
+    std::cout << "Hello, World!";
+}
+
+| Col1 | Col2 |
+|------|------|
+| A    | B    |
+| C    | D    |
+\author Strazimiri Besim    
+\date 2025
+\version 1.0
+
+
--- carre.h
+++ carre.h
@@ -1,20 +1,38 @@
 #pragma once 
+/// @brief Programme de gestion d'un carré
+/// @file carre.h
+/// @date 2024/01/08
+/// @version 1.0
+/// @author Strazimiri Besim
 
 class CCarre {
+	/// @brief Classe CCarre
 private:
+	/// @brief Attributs de la classe CCarre
 	int sx;
 	int sy;
 	int cote;
 
-
+	/// @brief Méthodes de la classe CCarre
 public:
+	/// @brief 
+	/// @param sx 
+	/// @param sy 
 	CCarre(int sx, int sy);
+	/// @brief Constructeur par défaut
 	CCarre();
+	/// @brief 
+	/// @param sx1 
 	void Setsx(int sx1);
+	/// @brief 
+	/// @param sy1 
 	void Setsy(int sy1);
+	/// @brief
+	/// @param cote1 
 	void Setcote(int cote1);
 
-	
+	/// @brief
+	/// @return 
 	int Getsx();
 	int Getsy();
 	int GetCote();
@@ -25,6 +43,7 @@ public:
 	//  - direction : vaut 'n' si nord, 's' si sud, 'o' si ouest, 'e' si est 
 	//   - saut : nombre de pixels de déplacement du carré 
 	// Sortie : Aucune 
+	/// @brief 
 	void Deplacer(char direction, int saut);
 	void Deplacer(int dx, int dy);
 
